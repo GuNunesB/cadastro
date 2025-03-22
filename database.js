851f135438da491b19ec17405a60e04f5f0a326d
GuNunesB <gununes280@gmail.com>
2025-03-21 21:24:47 -0300

    atualização

--- database.js
+++ database.js
@@ -1,28 +1,15 @@
-/**
- * Módulo de conexão com o banco de dados
- * Uso do framework mongoose
- */
-
-// importação do mongoose
 const mongoose = require('mongoose')
 
-// configuração do banco de dados
-// ip/link do servidor, autenticação 
-// ao final da url definir o nome do banco de dados
-// exemplo: /dbnotes
 const url = 'mongodb+srv://admin:123Senac@cluster0.vi8rq.mongodb.net/'
 
-// validação (evitar a abertura de várias conexões)
+
 let conectado = false
 
-// método para conectar com o banco de dados
 const conectar = async () => {
-    // se não estiver conectado
     if (!conectado) {
-        //conectar com o banco de dados
         try {
-            await mongoose.connect(url) //conectar
-            conectado = true //setar a variável
+            await mongoose.connect(url)
+            conectado = true
             console.log("MongoDB conectado")
             return true
         } catch (error) {
@@ -32,14 +19,11 @@ const conectar = async () => {
     }
 }
 
-// método para desconectar do banco de dados
 const desconectar = async () => {
-    // se estiver conectado
     if (conectado) {
-        // desconectar
         try {
-            await mongoose.disconnect(url) //desconectar
-            conectado = false //setar a variável
+            await mongoose.disconnect(url)
+            conectado = false
             console.log("MongoDB desconectado")
             return true
         } catch (error) {
